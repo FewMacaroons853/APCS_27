158e834c7caaff16124907f6d698e2788832f176
Aiden Chun <109098567+FewMacaroons853@users.noreply.github.com>
2026-09-14 21:03:05 +0000

    commit

--- 200_Personal_Projects/ubc/starter.java
+++ 200_Personal_Projects/ubc/starter.java
@@ -13,7 +13,10 @@ class starter {
 		// Your code goes below here
 		BaseClass test = new BaseClass();
 
-
+		int num1 = (5-7*3); 
+		int num2 = (8%5*7);	
+		double myNumber = Math.max(num1,num2);
 		
+
 	}
 }
